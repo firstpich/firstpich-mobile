b5e882b96dda243e8d7867a701bab27a71126d59
spantheslayer <supriyosarkar282@gmail.com>
2022-02-14 00:17:40 +0530

    chore: added components folder with a test file

---
+++ components/MyComponent.tsx
@@ -0,0 +1,17 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {useTailwind} from 'tailwind-rn';
+
+export default function MyComponent() {
+  const tailwind = useTailwind();
+  return (
+    <View style={tailwind('flex-1 justify-center items-center')}>
+      <Text style={tailwind('text-green-400 text-3xl')}>
+        Hi Sandipan !!How are you?
+      </Text>
+      <Text style={tailwind('text-green-400 text-3xl')}>
+        Let's build firstpich
+      </Text>
+    </View>
+  );
+}
