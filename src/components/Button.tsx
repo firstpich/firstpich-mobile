ca64b2ee9c7227005b7fdb0722c41c42a54b6902
spantheslayer <supriyosarkar282@gmail.com>
2022-03-12 11:31:09 +0530

    feat: button component added

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import {useTailwind} from 'tailwind-rn';
+
+type ButtonProps = {
+  title: string;
+};
+
+const FpButton: React.FC<ButtonProps> = ({title}) => {
+  const tailwind = useTailwind();
+  return (
+    <TouchableOpacity>
+      <View
+        style={tailwind(
+          'flex bg-white w-72 h-11 rounded-3xl justify-center items-center',
+        )}>
+        <Text style={tailwind('text-black font-bold text-lg')}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default FpButton;
