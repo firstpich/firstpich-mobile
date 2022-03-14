ea76bbe8f03640eb029e03a3f6f15c03829609d4
Supriyo Sarkar <supriyosarkar282@gmail.com>
2022-03-15 02:40:17 +0530

    feat: added splash screen (#6)
    * feat: added splash screen for android
    
    * refactor: better folder structure, ts declaration fix
    
    * feat: splash screen on ios working
    
    Co-authored-by: Sandipan Dey <hey@sandipan.dev>

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React, {useEffect} from 'react';
+import {TailwindProvider} from 'tailwind-rn';
+
+import SplashScreen from 'react-native-splash-screen';
+
+import Home from './pages/Home';
+
+import utilities from '../tailwind.json';
+
+export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <TailwindProvider utilities={utilities}>
+      <Home />
+    </TailwindProvider>
+  );
+}
---
+++ src/pages/Home.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, ImageBackground} from 'react-native';
+import {useTailwind} from 'tailwind-rn';
+
+import FpLogo from '../../assets/icons/fpLogo.svg';
+import BgShade from '../../assets/icons/bgShade.png';
+
+const Home = () => {
+  const tailwind = useTailwind();
+  return (
+    <React.Fragment>
+      <View style={tailwind('bg-primary relative h-full')}>
+        <View
+          style={tailwind(
+            'absolute w-full flex h-full items-center justify-center',
+          )}>
+          <FpLogo width={40} height={40} />
+          <Text style={tailwind('text-white font-mon-bold text-2xl')}>
+            firstpich
+          </Text>
+        </View>
+        <ImageBackground
+          source={BgShade}
+          style={{
+            ...tailwind('flex-1 justify-center opacity-20'),
+          }}
+        />
+      </View>
+    </React.Fragment>
+  );
+};
+
+export default Home;
