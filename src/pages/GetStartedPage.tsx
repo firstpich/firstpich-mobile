5dbd96dcd4808d91567789f9bad383b5bddfcfb4
spantheslayer <supriyosarkar282@gmail.com>
2022-03-17 19:15:09 +0530

    chore: fixed file issue

--- src/pages/GetStartedPage.tsx
+++ src/pages/GetStartedPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import {View, Text, ImageBackground} from 'react-native';
 import {useTailwind} from 'tailwind-rn';
 
@@ -17,15 +16,6 @@ type GetStartedNavigationProps = NativeStackNavigationProp<
 
 const GetStartedPage = () => {
   const navigation = useNavigation<GetStartedNavigationProps>();
-=======
-import {View, Text, ImageBackground, Alert} from 'react-native';
-import {useTailwind} from 'tailwind-rn';
-
-import GetStartedBg from '../../assets/icons/getStartedBg.png';
-import FpButton from '../components/Button';
-
-const Home = () => {
->>>>>>> 3dc0a1e (feat: made get started page (#9))
   const tailwind = useTailwind();
 
   return (
@@ -38,40 +28,25 @@ const Home = () => {
               firstpich
             </Text>
           </View>
-<<<<<<< HEAD
           <View style={tailwind('flex-1 items-center justify-center -mt-56')}>
             <Text
               style={tailwind(
                 'ml-3 text-white w-48 text-center font-mon-bold text-lg',
-=======
-          <View style={tailwind('flex-1 items-center justify-center -mt-24')}>
-            <Text
-              style={tailwind(
-                'ml-3 text-white w-48 text-center font-mon-bold',
->>>>>>> 3dc0a1e (feat: made get started page (#9))
               )}>
               Stories just for you to listen & read
             </Text>
           </View>
         </View>
         <View style={tailwind('absolute w-full bottom-6')}>
-<<<<<<< HEAD
           <FpButton
             title="Get Started"
             className="mx-4"
             onPress={() => navigation.navigate('SignUp')}
           />
-=======
-          <FpButton title="Get Started" className="mx-4" />
->>>>>>> 3dc0a1e (feat: made get started page (#9))
         </View>
       </ImageBackground>
     </View>
   );
 };
 
-<<<<<<< HEAD
 export default GetStartedPage;
-=======
-export default Home;
->>>>>>> 3dc0a1e (feat: made get started page (#9))
