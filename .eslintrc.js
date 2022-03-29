874c54d481e93a608144a76794e77b05e2a1c97a
Sandipan Dey <hey@sandipan.dev>
2022-03-30 01:01:43 +0530

    chore: eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,10 @@ module.exports = {
   extends: "@react-native-community",
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "react", "react-hooks", "prettier"],
-  singleQuote: false,
-  trailingComma: "all",
-  printWidth: 150,
   rules: {
     "no-shadow": "off",
     "@typescript-eslint/no-shadow": ["error"],
     quotes: [2, "double", { avoidEscape: true }],
+    "prettier/prettier": ["error", { singleQuote: false }],
   },
 };
