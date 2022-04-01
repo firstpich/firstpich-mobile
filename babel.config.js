783b358e5114d488961d11893cfe04c6c7af19d3
Sandipan Dey <hey@sandipan.dev>
2022-04-02 02:53:37 +0530

    chore: using atrate syntax for imports

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    [
+      require.resolve("babel-plugin-module-resolver"),
+      {
+        cwd: "babelrc",
+        extensions: [".ts", ".tsx", ".js", ".ios.js", ".android.js"],
+        alias: {
+          "@assets": "./assets",
+          "@components": "./src/components",
+          "@db": "./src/db",
+          "@validators": "./src/validators",
+          "@src": "./src",
+        },
+      },
+    ],
+  ],
 };
