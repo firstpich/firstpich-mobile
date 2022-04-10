e577268c4e2476402ba30ed2a7196781093eed95
spantheslayer <supriyosarkar282@gmail.com>
2022-04-10 18:25:52 +0530

    wip: added nextChapter card intial mockup

--- src/pages/book/BookPlayer.tsx
+++ src/pages/book/BookPlayer.tsx
@@ -3,6 +3,7 @@ import { SafeAreaView, ScrollView, StatusBar, Text, View } from "react-native";
 import { useTailwind } from "tailwind-rn";
 
 import BookPageHeader from "@components/book/BookPageHeader";
+import NextChapter from "@components/book/NextChapter";
 
 const Create = () => {
   const tailwind = useTailwind();
@@ -21,54 +22,44 @@ const Create = () => {
           className="font-mon-light text-sm"
         />
       </View>
-      <ScrollView
+      <View
         onLayout={e => {
           console.log(e.nativeEvent.layout);
         }}
-        style={tailwind("flex-grow px-4 py-4")}>
+        style={tailwind("")}>
         <Text
-          style={tailwind(
-            "font-mon-regular text-white text-lg text-left bg-red-500",
-          )}>
-          Lorem adipisicing velit anim incididunt non veniam irure aute sint ut
-          magna sunt sint consectetur. Exercitation irure aliqua ex
-          reprehenderit enim duis ad. Non sit in anim sit Lorem labore proident
-          aute sunt est veniam fugiat culpa aute. Quis eu nulla aute proident
-          sint aute non minim nulla sunt consectetur nisi. Dolore irure quis
-          velit do. Mollit ex ad veniam eiusmod mollit ex consectetur sit sunt
-          nisi. Proident in veniam ipsum Lorem irure. Pariatur eiusmod aute amet
-          veniam ut. Cillum adipisicing elit proident elit occaecat id. Eu ad
-          dolor ea laboris incididunt voluptate occaecat aliqua est deserunt.
-          Magna anim mollit sunt cillum dolore id nulla. Veniam velit excepteur
-          aliqua minim. Ullamco commodo officia enim officia quis velit minim
-          mollit magna. Irure velit sit adipisicing nisi adipisicing irure
-          ullamco et ut velit officia. Lorem nisi sunt id labore ad adipisicing
-          ea aliqua ex labore. Lorem minim consequat id cupidatat magna Lorem
-          cupidatat incididunt do dolore id. Culpa commodo fugia t commodo ea
-          laboris est irure qui laborum excepteur id incididunt in. Cillum
-          exercitation qui laboris commodo enim. Laborum mollit laborum velit
-          consequat excepteur tempor qui anim et exercitation consequat. Lorem
-          adipisicing velit anim incididunt non veniam irure aute sint ut magna
-          sunt sint consectetur. Exercitation irure aliqua ex reprehenderit enim
-          duis ad. Non sit in anim sit Lorem labore proident aute sunt est
-          veniam fugiat culpa aute. Quis eu nulla aute proident sint aute non
-          minim nulla sunt consectetur nisi. Dolore irure quis velit do. Mollit
-          ex ad veniam eiusmod mollit ex consectetur sit sunt nisi. Proident in
-          veniam ipsum Lorem irure. Pariatur eiusmod aute amet veniam ut. Cillum
-          adipisicing elit proident elit occaecat id. Eu ad dolor ea laboris
-          incididunt voluptate occaecat aliqua est deserunt. Magna anim mollit
-          sunt cillum dolore id nulla. Veniam velit excepteur aliqua minim.
-          Ullamco commodo officia enim officia quis velit minim mollit magna.
-          Irure velit sit adipisicing nisi adipisicing irure ullamco et ut velit
-          officia. Lorem nisi sunt id labore ad adipisicing ea aliqua ex labore.
-          Lorem minim consequat id cupidatat magna Lorem cupidatat incididunt do
-          dolore id. Culpa commodo fugiat commodo ea laboris est irure qui
-          laborum excepteur id incididunt in. Cillum exercitation qui laboris
-          commodo enim. Laborum mollit laborum velit consequat excepteur tempor
-          qui anim et exercitation consequat.
+          style={{
+            ...tailwind(
+              "flex-grow font-mon-regular text-white text-base text-justify bg-red-500",
+            ),
+          }}>
+          He groaned as an elixir, laced with the flavor of rosemary and sage,
+          hit his mouth, his tongue, then trickled like the most beautiful fire
+          down his throat. He formed a seal around the wounds and began to suck.
+          He lost a sense of time and place. All he felt was his cock driving
+          into her once more, her body moving in heavy waves beneath his, and
+          the amplified nature of her blood powering him once more. He went
+          faster, a strong dedicated rhythm. Griffin. I love you. His heart
+          swelled and his mating vibration began to pulse, a demanding frequency
+          that called to her. He groaned as an elixir, laced with the flavor of
+          rosemary and sage, hit his mouth, his tongue, then trickled like the
+          most beautiful fire down his throat. He formed a seal around the
+          wounds and began to suck. He lost a sense of time and place. All he
+          felt was his cock driving into her once more, her body moving in heavy
+          waves beneath his, and the amplified nature of her blood powering him
+          once more. He went faster, a strong dedicated rhythm. Griffin. I love
+          you. His heart swelled and his mating vibration began to pulse, a
+          demanding frequency that called to her. He groaned as an elixir, laced
+          with the flavor of rosemary and sage, hit his mouth, his tongue, then
+          trickled like the most beautiful fire down his throat. He formed a
+          seal around the wounds and began to suck. He lost a sense of time and
+          place. All he felt was his cock driving into her once more, her body
+          moving in heavy waves beneath his, and the amplified nature of her
+          blood powering him once more.
         </Text>
-      </ScrollView>
-      <View style={tailwind("flex-grow py-12")}>
+      </View>
+      <View style={tailwind("mx-auto ")}>
+        <NextChapter />
         <Text style={tailwind("text-center")}>69</Text>
       </View>
     </SafeAreaView>
