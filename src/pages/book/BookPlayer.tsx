834c7cdba2d85942721a740047a4b2b052ab1d79
spantheslayer <supriyosarkar282@gmail.com>
2022-04-10 13:12:42 +0530

    wip: updated len val

--- src/pages/book/BookPlayer.tsx
+++ src/pages/book/BookPlayer.tsx
@@ -21,9 +21,15 @@ const Create = () => {
           className="font-mon-light text-sm"
         />
       </View>
-      <ScrollView style={tailwind("flex-grow px-4 py-4")}>
+      <ScrollView
+        onLayout={e => {
+          console.log(e.nativeEvent.layout);
+        }}
+        style={tailwind("flex-grow px-4 py-4")}>
         <Text
-          style={tailwind("font-mon-regular text-white text-lg text-left ")}>
+          style={tailwind(
+            "font-mon-regular text-white text-lg text-left bg-red-500",
+          )}>
           Lorem adipisicing velit anim incididunt non veniam irure aute sint ut
           magna sunt sint consectetur. Exercitation irure aliqua ex
           reprehenderit enim duis ad. Non sit in anim sit Lorem labore proident
@@ -38,10 +44,28 @@ const Create = () => {
           mollit magna. Irure velit sit adipisicing nisi adipisicing irure
           ullamco et ut velit officia. Lorem nisi sunt id labore ad adipisicing
           ea aliqua ex labore. Lorem minim consequat id cupidatat magna Lorem
-          cupidatat incididunt do dolore id. Culpa commodo fugiat commodo ea
+          cupidatat incididunt do dolore id. Culpa commodo fugia t commodo ea
           laboris est irure qui laborum excepteur id incididunt in. Cillum
           exercitation qui laboris commodo enim. Laborum mollit laborum velit
-          consequat excepteur tempor qui anim et exercitation consequat.
+          consequat excepteur tempor qui anim et exercitation consequat. Lorem
+          adipisicing velit anim incididunt non veniam irure aute sint ut magna
+          sunt sint consectetur. Exercitation irure aliqua ex reprehenderit enim
+          duis ad. Non sit in anim sit Lorem labore proident aute sunt est
+          veniam fugiat culpa aute. Quis eu nulla aute proident sint aute non
+          minim nulla sunt consectetur nisi. Dolore irure quis velit do. Mollit
+          ex ad veniam eiusmod mollit ex consectetur sit sunt nisi. Proident in
+          veniam ipsum Lorem irure. Pariatur eiusmod aute amet veniam ut. Cillum
+          adipisicing elit proident elit occaecat id. Eu ad dolor ea laboris
+          incididunt voluptate occaecat aliqua est deserunt. Magna anim mollit
+          sunt cillum dolore id nulla. Veniam velit excepteur aliqua minim.
+          Ullamco commodo officia enim officia quis velit minim mollit magna.
+          Irure velit sit adipisicing nisi adipisicing irure ullamco et ut velit
+          officia. Lorem nisi sunt id labore ad adipisicing ea aliqua ex labore.
+          Lorem minim consequat id cupidatat magna Lorem cupidatat incididunt do
+          dolore id. Culpa commodo fugiat commodo ea laboris est irure qui
+          laborum excepteur id incididunt in. Cillum exercitation qui laboris
+          commodo enim. Laborum mollit laborum velit consequat excepteur tempor
+          qui anim et exercitation consequat.
         </Text>
       </ScrollView>
       <View style={tailwind("flex-grow py-12")}>
